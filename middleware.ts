7a7b59c1e75e4bd726b87c259441a391e9928652
Ann-Annice <annannice98@gmail.com>
2026-07-12 18:40:20 +0530

    Change malware

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
-
-  console.log("TOKEN:", token);
-
-  if (
-    request.nextUrl.pathname === "/login" ||
-    request.nextUrl.pathname === "/register"
-  ) {
-    return NextResponse.next();
-  }
-
-  if (!token) {
-    console.log("NO TOKEN");
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  try {
-    const decoded = jwt.verify(
-      token,
-      process.env.AUTH_SECRET!
-    );
-
-    console.log(decoded);
-
-    return NextResponse.next();
-  } catch (err) {
-    console.log("JWT ERROR:", err);
-
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+export function middleware() {
+  return NextResponse.next();
 }
 
 export const config = {
